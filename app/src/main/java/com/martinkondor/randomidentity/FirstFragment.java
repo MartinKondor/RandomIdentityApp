c90414f56b61a4c1da6f62aa75b643edf9ae23c8
Martin Kondor <martinkondor@gmail.com>
2022-02-04 12:52:57 +0100

    Initial commit

---
+++ app/src/main/java/com/martinkondor/randomidentity/FirstFragment.java
@@ -0,0 +1,139 @@
+package com.martinkondor.randomidentity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+
+import java.time.Year;
+import java.time.ZoneId;
+import java.util.Calendar;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import android.graphics.BitmapFactory;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.martinkondor.randomidentity.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private Generator g;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        g = new Generator(getResources());  // Load generator
+
+        binding.generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { generateNewId(getActivity()); }
+        });
+    }
+
+    private void generateNewId(FragmentActivity ac) {
+        boolean isMale = g.getInt(100) % 2 == 0;
+        String chosenCountry = "Hungary";
+
+        // Get new image if possible
+        ImageView image = (ImageView) getActivity().findViewById(R.id.image);
+        image.setImageResource(isMale ? R.drawable.male : R.drawable.female);
+
+        TextView name = (TextView) ac.findViewById(R.id.name);
+        TextView address = (TextView) ac.findViewById(R.id.address);
+        TextView city = (TextView) ac.findViewById(R.id.city);
+        TextView mother = (TextView) ac.findViewById(R.id.mother);
+        TextView phone = (TextView) ac.findViewById(R.id.phone);
+        TextView countryCode = (TextView) ac.findViewById(R.id.countryCode);
+        TextView birthday = (TextView) ac.findViewById(R.id.birthday);
+        //TextView age = (TextView) ac.findViewById(R.id.age);
+        TextView email = (TextView) ac.findViewById(R.id.email);
+        TextView username = (TextView) ac.findViewById(R.id.username);
+        TextView password = (TextView) ac.findViewById(R.id.password);
+
+        String fullName = g.getFullName(isMale);
+        TextView[] textViews = new TextView[] {
+                name, address, city, mother, phone, countryCode,
+                birthday, email, username, password
+        };
+
+        name.setText(fullName);
+        address.setText(g.getAddress());
+        city.setText(g.getCity(chosenCountry));
+        mother.setText(g.getFullName(false));
+        phone.setText(g.getPhone());
+        countryCode.setText(g.getCountryCode(chosenCountry));
+        birthday.setText(g.getBirthday());
+        //age.setText(Year.now().getValue());// - Integer.parseInt(bday.split(",")[1].trim()));
+        email.setText(g.getEmail(fullName));
+        username.setText(g.getUsername(fullName));
+        password.setText(g.getPassword());
+
+        ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+
+        // Set click listeners
+        for (TextView t : textViews) {
+            t.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // Copy text on click
+                    ClipData clip = ClipData.newPlainText("copied fake data", t.getText());
+                    clipboard.setPrimaryClip(clip);
+
+                    // Show that text is copied now
+                    Snackbar.make(getActivity().findViewById(android.R.id.content),
+                            "Text copied to clipboard", Snackbar.LENGTH_LONG)
+                            .setAction("CLOSE", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {}
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                            .show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
+
+
---
+++ app/src/main/java/com/martinkondor/randomidentity/Generator.java
@@ -0,0 +1,221 @@
+package com.martinkondor.randomidentity;
+
+import android.content.res.Resources;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
+
+class Generator {
+
+    protected List<String> lastNames = new ArrayList<String>();
+    protected List<String> maleFirstNames = new ArrayList<String>();
+    protected List<String> femaleFirstNames = new ArrayList<String>();
+
+    // Country name: country phone code
+    protected Map<String, String> countries = new HashMap<>();
+
+    // Country name: city list
+    protected Map<String, List<String>> cities = new HashMap<>();
+
+    // City name: county name
+    protected Map<String, String> cityCounties = new HashMap<>();
+
+    protected String[] months = new String[] {
+            "January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December"
+    };
+    protected int[] monthDays = new int[] {31, 28, 31, 29, 31, 30, 29, 31, 30, 31, 30, 31};
+    protected String[] emailDomains = new String[] {
+            "@armyspy.com", "@dayrep.com", "@einrot.com", "@fleckens.hu", "@gustr.com",
+            "@jourrapide.com", "@rhyta.com", "@superrito.com", "@teleworm.us"
+    };
+
+    public Generator(Resources res) {
+        // Load lists
+        try {
+            InputStreamReader isr = new InputStreamReader(res.openRawResource(R.raw.male_names));
+            BufferedReader fr = new BufferedReader(isr);
+            String line = null;
+            do {
+                line = fr.readLine();
+                if (line == null) break;
+                maleFirstNames.add(line.split(",")[1]);
+            } while (line != null);
+            isr.close();
+            fr.close();
+
+            isr = new InputStreamReader(res.openRawResource(R.raw.female_names));
+            fr = new BufferedReader(isr);
+            line = null;
+            do {
+                line = fr.readLine();
+                if (line == null) break;
+                femaleFirstNames.add(line.split(",")[1]);
+            } while (line != null);
+            isr.close();
+            fr.close();
+
+            isr = new InputStreamReader(res.openRawResource(R.raw.last_names));
+            fr = new BufferedReader(isr);
+            line = null;
+            do {
+                line = fr.readLine();
+                if (line == null) break;
+                lastNames.add(line.split(",")[0]);
+            } while (line != null);
+            isr.close();
+            fr.close();
+
+            isr = new InputStreamReader(res.openRawResource(R.raw.country_phone_codes));
+            fr = new BufferedReader(isr);
+            line = null;
+            do {
+                line = fr.readLine();
+                if (line == null) break;
+                String[] data = line.split(",");
+                countries.put(data[2], data[1]);
+            } while (line != null);
+            isr.close();
+            fr.close();
+
+            isr = new InputStreamReader(res.openRawResource(R.raw.cities));
+            fr = new BufferedReader(isr);
+            line = null;
+            do {
+                line = fr.readLine();
+                if (line == null) break;
+                String[] data = line.split(",");
+
+                // If there is no such country in the map
+                if (!cities.keySet().contains(data[1])) {
+                    List<String> temp = new ArrayList<String>();
+                    temp.add(data[0]);
+                    cities.put(data[1], temp);
+                }
+                else {
+                    cities.get(data[1]).add(data[0]);
+                }
+                cityCounties.put(data[0], data[2]);
+            } while (line != null);
+            isr.close();
+            fr.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected int getInt(int max) {
+        return new Random().nextInt(max);
+    }
+
+    protected int getInt(int max, int min) {
+        return new Random().nextInt(max - min) + min;
+    }
+
+    protected List<String> getCountryNames(Resources res) {
+        List<String> list = new ArrayList<String>();
+        for (String c : countries.keySet()) {
+            list.add(c);
+        }
+        return list;
+    }
+
+    protected String getPhone() {
+        return getInt(999, 10) + "-"
+                + getInt(999, 100) + "-"
+                + getInt(9999, 1000);
+    }
+
+    protected String getCountryCode(String country) {
+        return "+" + countries.get(country);
+    }
+
+    protected String getName(boolean isMale) {
+        return isMale ? choice(maleFirstNames) : choice(femaleFirstNames);
+    }
+
+    protected String getFullName(boolean isMale) {
+        return getName(isMale) + " " + choice(lastNames);
+    }
+
+    protected String getCity(String country) {
+        if (cities.keySet().contains(country)) {
+            String city = choice(cities.get(country));
+            return city + ",\n" + cityCounties.get(city);
+        }
+        return "None";
+    }
+
+    protected String getAddress() {
+        return getFullName(getInt(100) % 2 == 0) + " " + getInt(1000);
+    }
+
+    protected String getBirthday() {
+        int i = getInt(months.length - 1);
+        return months[i] + " " + getInt(monthDays[i]) + ", " + getInt(2022, 1947);
+    }
+
+    protected String getEmail(String fullName) {
+        String[] name = fullName.toLowerCase().split(" ");
+        int seed = getInt(100);
+
+        if (seed < 9 && name[1].length() > 3) {
+            return name[0] + name[1].charAt(0) + name[1].charAt(1) + name[1].charAt(2) + choice(emailDomains);
+        }
+        else if (seed < 29) {
+            return name[getInt(10)%2==0 ? 1 : 0] + getInt(9999, 10) + choice(emailDomains);
+        }
+        else if (seed < 59) {
+            return name[getInt(10)%2==0 ? 1 : 0] + getInt(9999, 10) + choice(emailDomains);
+        }
+        else if (seed < 89) {
+            return name[getInt(10)%2==0 ? 1 : 0] + getInt(9999, 10) + choice(emailDomains);
+        }
+        return name[0] + name[1] + getInt(99) + choice(emailDomains);
+    }
+
+    protected String getUsername(String fullName) {
+        String[] name = fullName.toLowerCase().split(" ");
+        int seed = getInt(100);
+        if (seed < 9 && name[1].length() > 3) {
+            return name[0] + name[1].charAt(0) + name[1].charAt(1) + name[1].charAt(2);
+        }
+        else if (seed < 29) {
+            return name[getInt(10)%2==0 ? 1 : 0] + getInt(9999, 10);
+        }
+        else if (seed < 59) {
+            return name[getInt(10)%2==0 ? 1 : 0] + getInt(9999, 10);
+        }
+        else if (seed < 89) {
+            return name[getInt(10)%2==0 ? 1 : 0] + getInt(9999, 10);
+        }
+        return name[0] + name[1] + getInt(99);
+    }
+
+    protected String getPassword() {
+        String result = "";
+        for (int i = 0; i < getInt(16, 8); i++) {
+            result += getInt(9);
+        }
+        return result;
+    }
+
+    private String choice(List<String> l) {
+        return l.get(getInt(l.size()));
+    }
+
+    private String choice(String[] l) {
+        return l[getInt(l.length - 1)];
+    }
+}
